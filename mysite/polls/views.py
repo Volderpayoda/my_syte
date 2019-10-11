d80f0b3487e878e2e8f1fe67ddf007b30f42bf8d
volderpayoda <volderpayoda@gmail.com>
2019-10-11 19:23:07 -0300

    Agregando la el manejador de votos y vista de resultados

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 # Create your views here.
 
@@ -16,8 +17,25 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Volvemos a mostrar el formulario para votar
+        context = {
+            'question': question, 
+            'error_message': 'No seleccionaste una opción'
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Siempre retornar una redirección cuando se completa un POST
+        # Esto se hace para evitar que los datos se vuelvan a enviar si el usuario presiona el botón "Back"
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     return HttpResponse("You're voting on question %s." % question_id)
